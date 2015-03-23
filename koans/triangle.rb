5c8c11df1a533935a46da148f052f9bc3f5bf6e3
Ryan Spittler <ryanspittler@gmail.com>
2015-03-23 17:39:22 -0600

    Add isosceles option for triangles in triangle.rb
    - Again, it got the test to pass. No need to add more until this
      is committed.
    - Made the equilateral only return if it is. Basically, this is
      the right code for the first test. The second test will be fixed
      in the next commit. Probably.
    
    [#89653462]

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  :equilateral
+  if a == b && b == c
+    :equilateral
+  else
+    :isosceles
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
